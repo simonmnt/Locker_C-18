622055301929b0a800f4b30561a9517f9e49d09a
Simon Minet <minet.simon@gmail.com>
2017-04-23 01:28:51 +0200

    clean

--- js/maintest.js
+++ js/maintest.js
@@ -2,12 +2,12 @@ var loading = false;
 var canvas = document.getElementById('canvas');
 console.log(1);
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = 960;
 var ctx = canvas.getContext('2d');
 console.log(2);
 var tileset = new Tileset('./img/tileset.png');
 console.log(3);
-var laby = new Labyrinthe(mazes, 4);
+var laby = new Labyrinthe(mazes, 5);
 console.log(4);
 
 if(loading){

bae473fec1d7afd615efdda4a622b76c5d8b655b
Gael PHILIPPE <contact@gael-philippe.fr>
2017-04-23 01:25:06 +0200

    new: Peuplage de la classe MazeMask

--- js/MazeMask.js
+++ js/MazeMask.js
@@ -5,4 +5,27 @@
 function MazeMask() {
     "use strict";
 
+    this.x = 0;
+    this.y = 0;
+
+    this.mask = new Image();
+    this.mask.loaded = false;
+    this.mask.onload = function () {
+        this.loaded = true;
+    };
+    this.mask.src = 'img/MazeMask.png';
+
+    this.draw = function () {
+        ctx.globalCompositeOperation = 'destination-in';
+
+        if (this.mask.loaded) {
+            ctx.drawImage(this.mask,
+                0, 0,
+                this.mask.naturalWidth, this.mask.naturalHeight,
+                this.x, this.y,
+                this.mask.naturalWidth, this.mask.naturalHeight);
+        }
+
+        ctx.globalCompositeOperation = 'source-over';
+    };
 }

41e47ef23c3aab3f024328fc60276e0091860d8d
Simon Minet <minet.simon@gmail.com>
2017-04-22 22:00:44 +0200

    new: ajout methode getType()

--- js/case.js
+++ js/case.js
@@ -25,7 +25,8 @@ function Case(_x, _y, _type)
 	this.draw = function()
 	{
 		console.log("tileset.img "+this.img.x+" "+this.img.y+" "+32+32+this.x*32+this.y*32+32+32);
-		ctx.drawImage(tileset.img, this.img.x, this.img.y, 32, 32, this.x*8, this.y*8, 8, 8);
+		ctx.drawImage(tileset.img, this.img.x, this.img.y, 32, 32, this.x*32, this.y*32, 32, 32);
+		//ctx.drawImage(tileset.img, this.img.x, this.img.y, 8, 8, this.x*8, this.y*8, 8, 8);
 	};
 	
 	 /**
@@ -38,4 +39,8 @@ function Case(_x, _y, _type)
 		iciCase.img.x = tileset.coordX(_type);
 		iciCase.img.y = tileset.coordY(_type);
 	};
+
+	this.getType = function () {
+        return iciCase.type = _type
+    }
 }

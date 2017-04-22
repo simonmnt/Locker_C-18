5972490de29e1fa033323ad7547c1b0fc3cc365b
Mike <mick.bst@gmail.com>
2017-04-22 16:18:23 +0200

    new: mazes.js // chg: Labyrinthe.js

--- js/Labyrinthe.js
+++ js/Labyrinthe.js
@@ -1,3 +1,25 @@
-function Labyrinthe() {
-    
+function Labyrinthe(_mazes, _lvl) {
+	var iciLab = this;
+    this.cases = [];
+	if(_lvl<Object.keys(_mazes).length)
+	{
+		var matrice = _mazes["Niveau "+_lvl].matrice;
+		for (var k =0; k<matrice.length;k++)
+		{
+			for (var i = 0; i<matrice[k].length;i++)
+			{
+				var maCase = new Case(k,i,matrice[k][i]);
+				iciLab.cases.push(maCase);
+			}
+		}
+	};
+	
+	this.afficheCarte()
+	{
+		for(var k=0; k<this.cases.length;k++)
+		{
+			cases[k].draw()
+		}
+	}
+	
 }

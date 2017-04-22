74c3161f9a5b3eaa3a7bec52d0dc84385c42c17f
MAStephen <stenm64@gmail.com>
2017-04-22 23:55:18 +0200

    new: Constante

--- js/Constante.js
+++ js/Constante.js
@@ -4,9 +4,25 @@ console.log('chargé ctx');
 var niveau = 1;
 
 var hero = new Hero("img/fabwormtest.png", 50, 80);
+var t = 0;
+var canvasTextTime = new MultiLineText(100, 10, 10, "Temps : " + t++ + ".", 18, 1.25, 'white', 'left');
+
+// var level = 1;
+// var levelText = new MultiLineText(100, 10, 30, "Niveau : " + swapLevel() + "sur 6", 18, 1.25, 'white', 'left');
+
+// function swapLevel(){
+
+// };
+
+// Augmenter le time de 1 toute les 1s
+function time(){
+	canvasTextTime.text = "Temps : " + t++ + ".";
+}
+var intervalTime = setInterval(time, 1000);
+
 
 function background(){
-	ctx.fillStyle = "green";
+	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	console.log('background green');
 }
@@ -16,12 +32,11 @@ function begin(){
 	touch();
 	console.log('hero ready');
 	background();
-	//AffichageText();
+	canvasTextTime.draw();
 	hero.draw(ctx);		
 	}			
 }
 var intervalConstante = setInterval(begin, 33);
-
 
 
 

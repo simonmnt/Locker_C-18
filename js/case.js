e917eb24b29f5129970b8244ce27259f7b375acc
Mike <mick.bst@gmail.com>
2017-04-22 14:12:18 +0200

    chg Case.js
    Ajout du setType(_type)
    Toujours pas de commentaires

--- js/case.js
+++ js/case.js
@@ -1,14 +1,36 @@
-function Case(_x,_y,_imgx,_imgy,_type)
+var images = 
 {
+	'W':[x,y];
+	' ':[x,y];
+	'L':[x,y];
+	'I':[x,y];
+	'S':[x,y];
+	'_':[x,y];
+	'M':[x,y];
+};
+
+function Case(_x,_y,_type)
+{
+	var iciCase = this;
 	this.x = _x;//number
 	this.y = _y;//number
-	this.img = {x=_imgx; y=_imgy;}
+	this.img = {x = images[_type][0]; y = images[_type][1];} //Generés par la valeur de type
 	this.type = _type; //MUR:'W',CHEMIN:' ',ALLUMETTE:'L',ENTREE:'I',SORTIE:'S',SOL:'_',MONSTRE:'M'
-	this.libre;
+	//this.libre = (_type == '_')?true:false;
 	//------------
 	this.draw = function()
 	{
-		ctx.drawImage(tileset,this.img.x*32, this.img.y*32, 32,32,this.x*32, this.y*32,32,32);
+		ctx.drawImage(tileset,this.img.x, this.img.y, 32,32,this.x*32, this.y*32,32,32);
+	}
+	
+	this.setType(_type)
+	{
+		iciCase.type = _type;
+		iciCase.img.x = images[_type][0];
+		iciCase.img.y = images[_type][1];
 	}
 }
 
+/**
+ tileset correspond à l'ensemble des images du jeu réunies dans une seule image.
+ */

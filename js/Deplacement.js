c168f9f9c1b21b93b3871b9f584c0f0b7b2ef999
MAStephen <stenm64@gmail.com>
2017-04-22 14:07:26 +0200

    new: Creation de classe Deplacement pour le Hero

--- js/Deplacement.js
+++ js/Deplacement.js
@@ -1,3 +1,28 @@
-function Deplacement(){
+function DeplacementHero(){
 
+	var myKey = {};
+	addEventListener('keydown', function(event){
+		myKey[event.keyCode] = true;
+	})
+	addEventListener('keyup', function(event){
+		myKey[event.keyCode] = false;
+	})
+	function touch() {
+		if(myKey[38]&& hero.y > 0){
+				hero.y -= hero.speed;
+				console.log('fleche haut');
+		}
+		if(myKey[40]&&hero.y < 960-hero.Image.width){
+				hero.y += hero.speed;
+				console.log('fleche bas');
+		}
+		if(myKey[37]&&hero.x > 0){
+				hero.x -= hero.speed;
+				console.log('fleche droite');
+		}
+		if(myKey[39]&&hero.x < 640-hero.Image.width){
+				hero.x += hero.speed;
+				console.log('fleche gauche');
+		}
+	}
 }

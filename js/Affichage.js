6c5aacecabe89d3daf22061260132464e6ee4827
MAStephen <stenm64@gmail.com>
2017-04-22 14:44:22 +0200

    Delete: Affichage.js

--- js/Affichage.js
+++
@@ -1,7 +0,0 @@
-function Affichage() {
-    if(this.hero.pret)
-	ctx.drawImage(hero.Image, 0, 0, 32, 32, hero.x, hero.y, 32, 32);
-	animationHero();
-}
-
-var interval = SetInterval(Affichage(), 30);

2877bd71f9cbc9710718a8ea03bdc31ecd290654
MAStephen <stenm64@gmail.com>
2017-04-22 14:07:59 +0200

    new: Creation de classe Monster

--- js/Monster.js
+++ js/Monster.js
@@ -1,4 +1,32 @@
-function Monster(_x, _y) {
-    this.x = _x;
-    this.y = _y;
+// function Monsteri(src, _x, _y) {
+//     this.x = _x;
+//     this.y = _y;
+
+// 	this.image = new Image();
+// 	this.image.src = src;
+// 	this.image.onload = function () {
+// 		this.pret = true;
+// 		console.log('monstre charger');
+// 	}
+	
+
+// }
+
+// var monster1 = new Monster(src, _x, _y);
+
+// // Position monstre
+// function PositionMonstre(monstrei, _monstrei_Y){
+
+// monstrei.x = _monstrei_X - monstre1.image.width / 2;
+// monstrei.y = _monstrei_Y - monstre1.image.height / 2;
+
+// }
+
+function SwapCase(){
+	// We take monster;
+	var nbSwap = nbMonstre;
+// chopper toutes les cases sol!
+// si case vide, on peux mettre type monstre!
+// ca sera aleatoire !
+
 }
